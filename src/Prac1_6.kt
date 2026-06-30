35b495099e37d1a98d4a44414e6b7b9fc121fd59
prashantt-16 <prashanttisbusy@gmail.com>
2026-06-30 13:52:27 +0530

    Initial

--- src/Prac1_6.kt
+++ src/Prac1_6.kt
@@ -2,6 +2,10 @@ fun main(){
     val number1=readln().toInt()
     val number2=readln().toInt()
     println(add(number1,number2))
+    println(add(number1,number2))
 
 }
 fun add(number1: Int,number2: Int): Int{return number1+number2}
+fun subtract(number1: Int,number2: Int):Int{return number1-number2}
+fun multiply(number1: Int,number2: Int):Int{return number1*number2}
+fun divide(number1: Int,number2: Int):Int{return number1/number2}
