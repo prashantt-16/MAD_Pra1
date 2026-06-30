1cf0bb5c4dbb202e96114a54e2cdd7f85477878e
prashantt-16 <prashanttisbusy@gmail.com>
2026-06-30 13:24:14 +0530

    Initial

---
+++ src/Prac1_1.kt
@@ -0,0 +1,29 @@
+
+fun main(){
+    var i=16
+    var j:Int=26
+    var k:Float=5.2f
+    var a:Char='A'
+    var b:String="Kayne"
+    var c:Boolean=false
+    var d:Double=10.5
+    var f:Long=123456789
+    var g:Short=10
+    var h:Byte=8
+    val x=100
+    j=200
+
+    println("x="+x)
+    println("j="+j)
+    println("i= "+i)
+    println("k=" +k)
+    println("a="+a)
+    println("b="+b)
+    println("c="+c)
+    println("d="+d)
+    println("f="+f)
+    println("g="+g)
+    println("h="+h)
+    println("int= $j")
+
+}
---
+++ src/Prac1_2.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var i:Int=10
+    var j:Double=i.toDouble()
+    val s:String="1000"
+    val a:Int=s.toInt()
+    val b: Double=s.toDouble()
+    println(i)
+    println(j)
+    println(s)
+    println(b)
+    println(a)
+}
